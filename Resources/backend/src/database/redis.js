ab3a97b2152b76cec55d51983bdaf77a690268fc
mdaffrzk1 <mdaffar105@gmail.com>
2026-04-30 22:40:49 +0700

    fix: update db ssl setup and redis env url for vercel

--- Resources/backend/src/database/redis.js
+++ Resources/backend/src/database/redis.js
@@ -1,10 +1,11 @@
 const Redis = require("ioredis");
 
-const redis = new Redis({
-    port: 6379, // Port Redis
-    host: "127.0.0.1", // Host Redis
-    db: 0,
-
-});
+const redis = process.env.REDIS_URL 
+    ? new Redis(process.env.REDIS_URL) 
+    : new Redis({
+        port: 6379, // Port Redis Lokal
+        host: "127.0.0.1", // Host Redis Lokal
+        db: 0,
+    });
 
 module.exports = redis;

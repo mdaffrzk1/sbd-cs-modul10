c3feea0798c3d896ec9300e9ae834b87e57c909b
mdaffrzk1 <mdaffar105@gmail.com>
2026-05-03 09:11:31 +0700

    benerin database.js

--- Resources/backend/src/config/database.js
+++ Resources/backend/src/config/database.js
@@ -1,13 +1,21 @@
 const { Pool } = require('pg');
 
-const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
-  ssl: process.env.DB_HOST !== 'localhost' ? { rejectUnauthorized: false } : false
-});
+// Gunakan DATABASE_URL
+const poolConfig = process.env.DATABASE_URL 
+  ? { 
+      connectionString: process.env.DATABASE_URL, 
+      ssl: { rejectUnauthorized: false } 
+    }
+  : {
+      user: process.env.DB_USER,
+      host: process.env.DB_HOST,
+      database: process.env.DB_NAME,
+      password: process.env.DB_PASSWORD,
+      port: process.env.DB_PORT,
+      ssl: process.env.DB_HOST !== 'localhost' ? { rejectUnauthorized: false } : false
+    };
+
+const pool = new Pool(poolConfig);
 
 pool.on('connect', () => {
   console.log('Connected to PostgreSQL database');
